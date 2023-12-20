4a5bd0177fded236053e5f7b4a5aebbc6b91d746
Michael Redig <mredig@gmail.com>
2023-12-19 18:41:55 -0600

    (feat) add test for appending items to existing appcast

--- Tests/PKGAppcastGeneratorCoreTests/GeneratorTests.swift
+++ Tests/PKGAppcastGeneratorCoreTests/GeneratorTests.swift
@@ -21,6 +21,26 @@ class GeneratorTests: XCTestCase {
 		XCTAssertEqual(xmlDoc, jsonExpectation)
 	}
 
+	func testAppendAppcast() throws {
+		let directory = Bundle.module.url(forResource: "JSONAppend", withExtension: nil, subdirectory: "TestResources")!
+		let jsonStarterURL = Bundle.module.url(forResource: "expectedJSONResult", withExtension: "xml", subdirectory: "TestResources")!
+		let jsonStarterData = try Data(contentsOf: jsonStarterURL)
+		let jsonAppendExpectationURL = Bundle.module.url(forResource: "expectedJSONAppendResult", withExtension: "xml", subdirectory: "TestResources")!
+		let jsonAppendExpectation = try XMLDocument(contentsOf: jsonAppendExpectationURL)
+
+		let data = try PKGAppcastGeneratorCore.generateAppcast(
+			fromContentsOfDirectory: directory,
+			previousAppcastData: jsonStarterData,
+			channelTitle: "Appcast",
+			signatureGenerator: { _ in "Secured! jk"},
+			downloadURLPrefix: #URL("https://he.ho.hum/updates/"))
+
+		let xmlDoc = try XMLDocument(data: data)
+		Self.cleanXMLDates(in: xmlDoc)
+
+		XCTAssertEqual(xmlDoc, jsonAppendExpectation)
+	}
+
 	static func cleanXMLDates(in xmlDoc: XMLDocument) {
 		var theNode: XMLNode? = xmlDoc
 
